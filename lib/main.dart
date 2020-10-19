8d526f355e0981330230707e657e10f742036d9d
Vinícius Mussato <vinicius.mussato@gmail.com>
2020-10-19 14:53:33 -0300

    made some visual improvements

--- lib/main.dart
+++ lib/main.dart
@@ -26,9 +26,22 @@ class MyApp extends StatelessWidget {
         body: SizedBox(
           height: 300,
           child: Card(
+            margin: EdgeInsets.all(16),
           child: Column(children: [
-            Image.network('https://img.itdg.com.br/tdg/images/recipes/000/110/765/102462/102462_original.jpg?mode=crop&width=710&height=400'),
-            Text('Bolo de Laranja')
+            Stack(
+              children: <Widget>[
+                Image.network(
+                  'https://img.itdg.com.br/tdg/images/recipes/000/110/765/102462/102462_original.jpg', 
+                  fit: BoxFit.fill, 
+                  height: 268,
+                  ),
+                Positioned(
+                  bottom: 10,
+                  left: 10,
+                  child: Text('Bolo de Laranja', style: TextStyle(fontSize: 20))
+                )
+              ]
+            )
           ],)
         )
         ),
