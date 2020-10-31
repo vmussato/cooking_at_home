f54e316eaa31a171724b6e8cc24485afb206895f
Vinícius Mussato <vinicius.mussato@gmail.com>
2020-10-31 14:19:21 -0300

    melhorias na organização de código

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import './telas/home/home.dart';
+
 
 void main() {
   runApp(MyApp());
@@ -27,56 +29,3 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class Home extends StatelessWidget {
-  
-  @override
-  Widget build(BuildContext context) {
-    return _construirHome();
-  }
-
-  Widget _construirHome() {
-
-    return Scaffold(
-        body: _construirCard(),
-        appBar: _construirAppBar(),
-      );
-    
-  }
-
-  Widget _construirCard() {
-    return SizedBox(
-          height: 300,
-          child: Card(
-            margin: EdgeInsets.all(16),
-          child: Column(children: [
-            Stack(
-              children: <Widget>[
-                _construirImagemCard(),
-                _construirTextoCard()
-              ]
-            )
-          ],)
-        )
-        );
-  }
-
-  Widget _construirTextoCard() {
-    return Positioned(
-      bottom: 10,
-      left: 10,
-      child: Text('Bolo de Laranja', style: TextStyle(fontSize: 20))
-    );
-  }
-
-  Widget _construirImagemCard() {
-    return Image.network(
-      'https://img.itdg.com.br/tdg/images/recipes/000/110/765/102462/102462_original.jpg', 
-      fit: BoxFit.fill, 
-      height: 268,
-    );
-  }
-
-  Widget _construirAppBar() {
-    return AppBar(title: Text('Cozinhando em casa'));
-  }
-}
