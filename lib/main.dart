851e13cfdb18f7c9c889746077f806ae8f44bab1
Vinícius Mussato <vinicius.mussato@gmail.com>
2020-10-31 14:14:28 -0300

    melhorando a estrutura da home

--- lib/main.dart
+++ lib/main.dart
@@ -22,31 +22,61 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.deepOrange,
       ),
-      home: Scaffold(
-        body: SizedBox(
+      home: Home()
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return _construirHome();
+  }
+
+  Widget _construirHome() {
+
+    return Scaffold(
+        body: _construirCard(),
+        appBar: _construirAppBar(),
+      );
+    
+  }
+
+  Widget _construirCard() {
+    return SizedBox(
           height: 300,
           child: Card(
             margin: EdgeInsets.all(16),
           child: Column(children: [
             Stack(
               children: <Widget>[
-                Image.network(
-                  'https://img.itdg.com.br/tdg/images/recipes/000/110/765/102462/102462_original.jpg', 
-                  fit: BoxFit.fill, 
-                  height: 268,
-                  ),
-                Positioned(
-                  bottom: 10,
-                  left: 10,
-                  child: Text('Bolo de Laranja', style: TextStyle(fontSize: 20))
-                )
+                _construirImagemCard(),
+                _construirTextoCard()
               ]
             )
           ],)
         )
-        ),
-        appBar: AppBar(title: Text('Cozinhando em casa')),
-      )
+        );
+  }
+
+  Widget _construirTextoCard() {
+    return Positioned(
+      bottom: 10,
+      left: 10,
+      child: Text('Bolo de Laranja', style: TextStyle(fontSize: 20))
     );
+  }
+
+  Widget _construirImagemCard() {
+    return Image.network(
+      'https://img.itdg.com.br/tdg/images/recipes/000/110/765/102462/102462_original.jpg', 
+      fit: BoxFit.fill, 
+      height: 268,
+    );
+  }
+
+  Widget _construirAppBar() {
+    return AppBar(title: Text('Cozinhando em casa'));
   }
 }
