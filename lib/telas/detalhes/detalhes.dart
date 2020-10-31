6535c0a22c2b06528c2868b68e26577a4ded0328
Vinícius Mussato <vinicius.mussato@gmail.com>
2020-10-31 16:48:57 -0300

    Criando a estrutura da tela de detalhes

--- lib/telas/detalhes/detalhes.dart
+++ lib/telas/detalhes/detalhes.dart
@@ -4,7 +4,64 @@ class Detalhes extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return _construirAppBar();
+    return _construirDetalhes();
+  }
+
+  Widget _construirDetalhes() {
+    return Scaffold(
+      body: Column(
+        children: [
+          _construirImagemDetalhes('assets/images/ratatouille.jpg'),
+          _construirTituloDetalhes('Ratatouille'),
+          _construirLinhaIconesDetalhes('2-3 porções' ,'50min'),
+          _construirSubtituloDetalhes('Ingredientes'),
+          _construirTextoDetalhes('Aqui serão exibidos os ingredientes'),
+          _construirSubtituloDetalhes('Modo de Preparo'),
+          _construirTextoDetalhes('Aqui será exibido o modo de preparo')
+
+        ],),
+      appBar: _construirAppBar()
+    );
+  }
+
+  Widget _construirTituloDetalhes(titulo) {
+    return Text(titulo, style: TextStyle(
+      color: Colors.deepOrange, fontSize: 30
+    ),
+    );
+  }
+
+  Widget _construirImagemDetalhes(imagem) {
+    return Image.asset(imagem);
+  }
+
+  Widget _construirLinhaIconesDetalhes(rendimento, tempo_preparo) {
+    return Row(
+      children: <Widget> [
+        _construirColunaIconeDetalhes(Icons.restaurant, rendimento),
+        _construirColunaIconeDetalhes(Icons.timer, tempo_preparo),
+      ],
+    );
+  }
+
+  Widget _construirColunaIconeDetalhes(icone, texto) {
+      return Expanded(
+        child: Column(children: <Widget>[
+        Icon(icone, color: Colors.deepOrange),
+        Text(texto, style: TextStyle(color: Colors.deepOrange, fontWeight: FontWeight.bold)),
+      ],
+      )
+    );
+  }
+
+  Widget _construirSubtituloDetalhes(subtitulo) {
+    return Text(subtitulo, style: TextStyle(
+      fontSize: 20
+      ));
+  }
+
+  Widget _construirTextoDetalhes(texto) {
+    return Text(texto);
   }
 
   Widget _construirAppBar() {
